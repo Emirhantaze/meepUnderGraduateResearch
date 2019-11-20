8389574e268039aeb34b3034094b603d58293346
mucahitdemir <43001724+mucahitdemir@users.noreply.github.com>
2019-11-20 11:22:05 +0300

    a

--- goldabrobtionspectum/goldapsorbtiontest4.py
+++ goldabrobtionspectum/goldapsorbtiontest4.py
@@ -3,14 +3,15 @@ import meep as mp
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from meep.materials import Au
 cell = mp.Vector3(4,2)
 pml_layers=[mp.PML(0.5)]
 r=0.1
 sx=2
-Au = mp.Medium(index=math.pow(6.9,(1/2)))
+#Au = mp.Medium(index=math.pow(6.9,(1/2)))
 dpml=0.5
 minwavelength=0.1
-maxwavelength=0.11
+maxwavelength=1.1
 minf=1/maxwavelength
 maxf=1/minwavelength
 fcen = (maxf+minf)/2  
@@ -20,7 +21,7 @@ resolution = 40
 sources = [mp.Source(mp.GaussianSource(fcen,fwidth=df),
                      component=mp.Ez,   
                      center=mp.Vector3(-1.5,0,0),
-                     size=mp.Vector3(0,0.02,0))]
+                     size=mp.Vector3(0,0.040,0))]
 sim = mp.Simulation(cell_size=cell,
                     boundary_layers=pml_layers,
                     sources=sources,
@@ -31,17 +32,17 @@ innerside_with_flux = []
 lightside_with_flux = []
 otherside_with_flux = []
 
-for i in (np.arange(1,6)):
+for i in (np.arange(1,3)):
     r=i*0.02
     sim = mp.Simulation(cell_size=cell,
                     boundary_layers=pml_layers,
                     sources=sources,
                     resolution=resolution)
     lightside_fr = mp.FluxRegion(center=mp.Vector3(-0.5-r), 
-                             size=mp.Vector3(0,0.02,0))
+                             size=mp.Vector3(0,0.04,0))
     lightside = sim.add_flux(fcen, df ,nfreq,lightside_fr)
     otherside_fr = mp.FluxRegion(center=mp.Vector3(+0.5+r), 
-                             size=mp.Vector3(0,0.02,0))
+                             size=mp.Vector3(0,0.04,0))
     otherside = sim.add_flux(fcen, df ,nfreq,otherside_fr)
     innerside_fr = mp.FluxRegion(center=mp.Vector3(), 
                              size=mp.Vector3(0,0.02,0))
@@ -73,15 +74,13 @@ for i in (np.arange(1,6)):
     #a=a[(74+12):(249-75+12)]
     otherside_with_flux = np.append(otherside_with_flux,a)
     innerside_with_flux = np.append(innerside_with_flux,mp.get_fluxes(innerside))
-apsorbtions = np.divide(innerside_with_flux-(otherside_with_flux+lightside_with_flux),otherside_without_flux)#+np.divide(lightside_with_flux,otherside_without_flux)
+apsorbtions = np.divide((otherside_with_flux),otherside_without_flux)#+np.divide(lightside_with_flux,otherside_without_flux)
 
 
 wavelengths = np.linspace(1100,100,250) 
 plt.figure()
 plt.plot(wavelengths,apsorbtions[np.arange(0,250)],'r',label='absorbtion 20')
 plt.plot(wavelengths,apsorbtions[np.arange(250,500)],'g',label='absorbtion 40')
-plt.plot(wavelengths,apsorbtions[np.arange(500,750)],'b',label='absorbtion 60')
-plt.plot(wavelengths,apsorbtions[np.arange(750,1000)],'y',label='absorbtion 80')
-plt.plot(wavelengths,apsorbtions[np.arange(1000,1250)],'p',label='absorbtion 100')
+
 plt.legend(loc="upper right")
 plt.show()
