8a0f766642f1f37d953748883ead048e3b8d9c6c
mucahitdemir <43001724+mucahitdemir@users.noreply.github.com>
2019-11-24 20:42:58 +0300

    Revert "absorbtion analyse"
    This reverts commit 1104cc91aa0739837287bdabae5ebd8fda94cb9e.

--- goldabrobtionspectum/goldapsorbtiontest4.py
+++ goldabrobtionspectum/goldapsorbtiontest4.py
@@ -4,9 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from meep.materials import Au
-
+cell = mp.Vector3(0.12,0.12)
 pml_layers=[mp.PML(0.01)]
-
+r=0.1
+sx=0.12
 #Au = mp.Medium(index=math.pow(6.9,(1/2)))
 dpml=0.01
 minwavelength=0.4
@@ -17,49 +18,42 @@ fcen = (maxf+minf)/2
 df = maxf-minf
 nfreq=100
 resolution = 100
+sources = [mp.Source(mp.GaussianSource(fcen,fwidth=df),
+                     component=mp.Ez,   
+                     center=mp.Vector3(-0.05,0,0),
+                     size=mp.Vector3(0,0.10,0))]
+sim = mp.Simulation(cell_size=cell,
+                    boundary_layers=pml_layers,
+                    sources=sources,
+                    resolution=resolution)
 
-
-innerside_without_flux = []
 otherside_without_flux = []
 innerside_with_flux = []
 lightside_with_flux = []
 otherside_with_flux = []
-indecentflux = []
 
-for i in (np.arange(1,5)):
-    r=i*0.005
-    sx=12*r+2*dpml
-    cell = mp.Vector3(sx,sx)
-    sources = [mp.Source(mp.GaussianSource(fcen,fwidth=df),
-                     component=mp.Ey,   
-                     center=mp.Vector3(-0.05*(sx-2*dpml),0,0),
-                     size=mp.Vector3(0,sx,0))]
+for i in (np.arange(1,3)):
+    r=i*0.01
     sim = mp.Simulation(cell_size=cell,
                     boundary_layers=pml_layers,
                     sources=sources,
                     resolution=resolution)
-    lightside_fr = mp.FluxRegion(center=mp.Vector3(-2*r), 
-                             size=mp.Vector3(0,4*r,0))
+    lightside_fr = mp.FluxRegion(center=mp.Vector3(-0.025), 
+                             size=mp.Vector3(0,0.05,0))
     lightside = sim.add_flux(fcen, df ,nfreq,lightside_fr)
-    otherside_fr = mp.FluxRegion(center=mp.Vector3(2*r), 
-                             size=mp.Vector3(0,4*r,0))
+    otherside_fr = mp.FluxRegion(center=mp.Vector3(+0.025), 
+                             size=mp.Vector3(0,0.05,0))
     otherside = sim.add_flux(fcen, df ,nfreq,otherside_fr)
-    innerside_fr = mp.FluxRegion(center=mp.Vector3(0,2*r), 
-                             size=mp.Vector3(4*r,0,0),
-                             )
-    incedent_fr = mp.FluxRegion(center=mp.Vector3(0.5*sx-dpml-0.01),size= mp.Vector3(0,sx))
-    incedent = sim.add_flux(fcen, df ,nfreq,incedent_fr)
+    innerside_fr = mp.FluxRegion(center=mp.Vector3(0,0.025), 
+                             size=mp.Vector3(0.05,0,0))
     innerside = sim.add_flux(fcen, df ,nfreq,innerside_fr)
     pt = mp.Vector3(0.5*sx-dpml-0.01,0)
     sim.run(until_after_sources=mp.stop_when_fields_decayed(50,mp.Ez,pt,1e-3))
     lightside_without_flux_data = sim.get_flux_data(lightside)
     innerside_without_flux_data = sim.get_flux_data(innerside)
-    otherside_without_flux_data= sim.get_flux_data(otherside)
-    indecentflux = np.append(indecentflux,mp.get_fluxes(incedent))
     a=mp.get_fluxes(otherside)
     #a=a[(74+12):(249-75+12)]
     otherside_without_flux = np.append(otherside_without_flux,a)
-    innerside_without_flux = np.append(innerside_without_flux,mp.get_fluxes(innerside))
     sim.reset_meep()
     geometrys = [mp.Cylinder(material=Au,radius=r,center=mp.Vector3())]
     sim = mp.Simulation(cell_size=cell,
@@ -71,9 +65,9 @@ for i in (np.arange(1,5)):
     lightside = sim.add_flux(fcen, df ,nfreq,lightside_fr)
     innerside = sim.add_flux(fcen, df ,nfreq,innerside_fr)
     otherside = sim.add_flux(fcen, df ,nfreq,otherside_fr)
-    #sim.load_minus_flux_data(otherside,otherside_without_flux_data)
+    
     sim.load_minus_flux_data(lightside,lightside_without_flux_data)
-    sim.load_minus_flux_data(innerside,innerside_without_flux_data)
+    #sim.load_minus_flux_data(innerside,innerside_without_flux_data)
     pt = mp.Vector3(0.5*sx-dpml-0.01,0)
     sim.run(until_after_sources=mp.stop_when_fields_decayed(50,mp.Ez,pt,1e-1))
     a=mp.get_fluxes(lightside)
@@ -84,22 +78,13 @@ for i in (np.arange(1,5)):
     
     otherside_with_flux = np.append(otherside_with_flux,a)
     innerside_with_flux = np.append(innerside_with_flux,mp.get_fluxes(innerside))
-absorbtions = np.divide((otherside_with_flux-lightside_with_flux-2*innerside_with_flux),otherside_without_flux)#+np.divide(lightside_with_flux,otherside_without_flux)
+apsorbtions = np.divide((otherside_with_flux-lightside_with_flux-2*innerside_with_flux),otherside_without_flux)#+np.divide(lightside_with_flux,otherside_without_flux)
 
 
-wavelengths = np.linspace(800,400,100) 
+wavelengths = np.linspace(1100,100,100) 
 plt.figure()
-<<<<<<< HEAD
-plt.plot(wavelengths,absorbtions[np.arange(0,100)],'r',label='absorbtion 20')
-plt.plot(wavelengths,absorbtions[np.arange(100,200)],'g',label='absorbtion 40')
-=======
-plt.plot(wavelengths,apsorbtions[np.arange(0,100)],'r',label='absorbtion 5')
-plt.plot(wavelengths,apsorbtions[np.arange(100,200)],'g',label='absorbtion 10')
-plt.plot(wavelengths,apsorbtions[np.arange(200,300)],'b',label='absorbtion 15')
-plt.plot(wavelengths,apsorbtions[np.arange(300,400)],'y',label='absorbtion 20')
+plt.plot(wavelengths,apsorbtions[np.arange(0,100)],'r',label='absorbtion 20')
+plt.plot(wavelengths,apsorbtions[np.arange(100,200)],'g',label='absorbtion 40')
 
->>>>>>> d91a969265bb9dadf409676e767cadad53e0d9fb
-
-plt.legend(loc="upper left")
-plt.grid()
+plt.legend(loc="upper right")
 plt.show()
